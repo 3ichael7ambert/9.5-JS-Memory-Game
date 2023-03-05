701bfce6c4349ba0171670843b8c57dded45bed1
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-03-05 13:52:03 -0700

    First commit

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 const gameContainer = document.getElementById("game");
+//const h1 = document.getElementByTag("h1");
+
+// let wiggles = true;
+// while (wiggle)
+// {
+//   setTimeout(h1.style.right="100px",1000);
+//   wiggle=false;
+// }
+// while (!wiggle)
+// {
+//   setTimeout(h1.style.left="100px",1000);
+//   wiggle=true;
+// }
+
+// function randomRGB() {
+
+//     const r = Math.floor(Math.random() *256);
+//     const g = Math.floor(Math.random() *256);
+//     const b = Math.floor(Math.random() *256);
+
+//     return `rgb(${r},${g},${b})`
+
+// }
+
+// const letters = document.querySelectorAll('.letter');
+
+// setInterval(function () {
+//     for (let letter of letters) {
+//         letter.style.color = randomRGB();
+
+//     }
+// },500)
+
 
 const COLORS = [
   "red",
