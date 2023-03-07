acbbacc95d2a241540b6af650e8bcb9807992369
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-03-06 19:20:50 -0700

    first commit, working flips, next to do is match

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ function shuffle(array) {
 
 let shuffledColors = shuffle(COLORS);
 
+
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
@@ -90,11 +91,111 @@ function createDivsForColors(colorArray) {
   }
 }
 
+// function createDivsForColors(colorArray) {
+//   for (let color of colorArray) {
+//     // create a new div
+//     const newDiv = document.createElement("div");
+//     const newFront = document.createElement("div");
+//     const newBack = document.createElement("div");
+//     // give it a class attribute for the value we are looping over
+//     newDiv.classList.add(color);
+//     newFront.className.add("front-face");
+//     newBack.className.add("back-face");
+//     // call a function handleCardClick when a div is clicked on
+//     newDiv.addEventListener("click", handleCardClick);
+
+//     // append the div to the element with an id of game
+//     gameContainer.append(newDiv);
+//     newDiv.append(newFront);
+//     newDiv.append(newBack);
+//   }
+// }
+
+function flipBack(event) {
+  event.target.style.background = "linear-gradient(olive,olivedrab)";
+}
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   console.log("you just clicked", event.target);
+  if (event.target.style.background="linear-gradient(olive,olivedrab"){
+  event.target.style.background = event.target.className;////////
+  }
+  // let cardBack = document.createElement("div");
+  //     cardBack.className = "back-face";
+  //     cardBack.style.background = event.target.className;
+  //     event.target.appendChild(cardBack);
+  // setTimeout(flipBack(event),1000);///////
+  // if (event.target.className==="front-face") {
+  //   event.target.className="back-face"
+  // } else if (event.target.className==="back-face") {
+  //   event.target.className="front-face"}
+    
 }
+
 
 // when the DOM loads
 createDivsForColors(shuffledColors);
+
+///
+//flipping effect
+///
+const cards = document.querySelectorAll('#game div');
+function flipCard() {
+  this.classList.toggle('flip');
+}
+// let hasFlippedCard = false;
+// let firstCard, secondCard;
+
+// const cards = document.querySelectorAll('#game div');
+// function flipCard() {
+//   this.classList.add('flip');
+
+//   if (!hasFlippedCard) {
+//     hasFlippedCard = true;
+//     firstCard = this;
+//    return;
+//  }
+
+//  secondCard = this;
+//  hasFlippedCard = false;
+
+//  checkForMatch();
+// }
+
+// function checkForMatch() {
+//  if (firstCard.classList === secondCard.classList {
+//    disableCards();
+//    return;
+//  }
+
+//  unflipCards();
+// }
+
+// function disableCards() {
+//  firstCard.removeEventListener('click', flipCard);
+//  secondCard.removeEventListener('click', flipCard);
+// }
+
+// function unflipCards() {
+//  setTimeout(() => {
+//    firstCard.classList.remove('flip');
+//    secondCard.classList.remove('flip');
+//  }, 1500);
+// }
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+///
+//Object to track if cards are flipped
+///
+// function cardsFlippedFunc(COLORS) {
+//   var cardsFlipped= {};
+//   for (var i = 0; i < COLORS.length; ++i)
+//     if (COLORS[i] !== undefined) cardsFlipped[i] = COLORS[i];
+//   return cardsFlipped;
+// }
+
+
